cc4527272951319613819ff808f816e7dc1e8b41
Mats Andreassen <matsand@live.no>
2021-01-24 19:59:04 +0100

    Oppdatert config

--- config.js
+++ config.js
@@ -1,10 +1,16 @@
 const { readFileSync } = require('fs')
 
 module.exports = {
-  url: process.env.KRR_URL || 'https://oidc-ver1.difi.no/',
-  cert: process.env.KRR_CERT ? Buffer.from(process.env.KRR_CERT, 'base64').toString() : readFileSync('./src/data/public.pem', 'utf-8'),
-  privateKey: process.env.KRR_PRIVATE_KEY ? Buffer.from(process.env.KRR_PRIVATE_KEY, 'base64').toString() : readFileSync('./src/data/private.key', 'utf-8'),
-  privateKeyPassphrase: process.env.KRR_PRIVATE_KEY_PASSPHRASE || undefined,
-  issuer: process.env.KRR_ISSUER || 'Din-id',
-  scope: process.env.KRR_SCOPE || 'global/kontaktinformasjon.read'
+  JWT_SECRET: process.env.JWT_SECRET || 'Very secret secret indeed',
+  KRR: {
+    URL: process.env.KRR_URL || 'https://krr.digdir.no/rest/v1/personer'
+  },
+  MASKINPORTEN: {
+    tokenUrl: process.env.MASKINPORTEN_TOKEN_URL || 'https://maskinporten.no/token',
+    audience: process.env.MASKINPORTEN_AUDIENCE || 'https://maskinporten.no/',
+    cert: process.env.MASKINPORTEN_CERT ? Buffer.from(process.env.KRR_CERT, 'base64').toString() : readFileSync('./data/public.pem', 'utf-8'),
+    privateKey: process.env.MASKINPORTEN_PRIVATE_KEY ? Buffer.from(process.env.KRR_PRIVATE_KEY, 'base64').toString() : readFileSync('./data/private.key', 'utf-8'),
+    issuer: process.env.MASKINPORTEN_ISSUER || '00000000-0000-0000-0000-000000000000',
+    scope: process.env.MASKINPORTEN_SCOPE || 'krr:global/kontaktinformasjon.read'
+  }
 }
