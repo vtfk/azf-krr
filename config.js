46571e703a1a5c13759d9363e71111a38b8ea04a
Mats Andreassen <matsand@live.no>
2021-01-24 20:14:30 +0100

    Oppdatert cert env var

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   MASKINPORTEN: {
     tokenUrl: process.env.MASKINPORTEN_TOKEN_URL || 'https://maskinporten.no/token',
     audience: process.env.MASKINPORTEN_AUDIENCE || 'https://maskinporten.no/',
-    cert: process.env.MASKINPORTEN_CERT ? Buffer.from(process.env.KRR_CERT, 'base64').toString() : readFileSync('./data/public.pem', 'utf-8'),
-    privateKey: process.env.MASKINPORTEN_PRIVATE_KEY ? Buffer.from(process.env.KRR_PRIVATE_KEY, 'base64').toString() : readFileSync('./data/private.key', 'utf-8'),
+    cert: process.env.MASKINPORTEN_CERT ? Buffer.from(process.env.MASKINPORTEN_CERT, 'base64').toString() : readFileSync('./data/public.pem', 'utf-8'),
+    privateKey: process.env.MASKINPORTEN_PRIVATE_KEY ? Buffer.from(process.env.MASKINPORTEN_PRIVATE_KEY, 'base64').toString() : readFileSync('./data/private.key', 'utf-8'),
     issuer: process.env.MASKINPORTEN_ISSUER || '00000000-0000-0000-0000-000000000000',
     scope: process.env.MASKINPORTEN_SCOPE || 'krr:global/kontaktinformasjon.read'
   }
